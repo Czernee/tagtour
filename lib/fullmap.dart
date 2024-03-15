9c40be0f147d9f464f0006c613cefad04c5885d2
Daniil Chernousov <i@tehnik-7.ru>
2024-03-15 21:59:12 +0300

    гавно ниче не работает просто тест

--- lib/fullmap.dart
+++ lib/fullmap.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'utils.dart';
+import 'siski.dart';
 
 class FullMap extends StatefulWidget {
   const FullMap({super.key});
@@ -10,43 +13,90 @@ class FullMap extends StatefulWidget {
 
 class FullMapState extends State<FullMap> {
   MapboxMap? mapboxMap;
+  PointAnnotation? pointAnnotation;
+  PointAnnotationManager? pointAnnotationManager;
 
   _onMapCreated(MapboxMap mapboxMap) {
     this.mapboxMap = mapboxMap;
-    this.mapboxMap?.setCamera(CameraOptions(
+
+    mapboxMap.setCamera(CameraOptions(
       center: Point(coordinates: Position(38.936694, 47.208735)).toJson(),
       zoom: 14
     ));
-    this.mapboxMap?.setBounds(CameraBoundsOptions(
-      bounds: CoordinateBounds(
-        //Юга запад-47.157511,38.828945
-        //Северо-восток-47.290974,38.956618
-        southwest: Point(
-          coordinates: Position(
-            38.828945,
-            47.157511,
-          )
-        ).toJson(),
-        northeast: Point(
-          coordinates: Position(
-            38.956618,
-            47.290974,
-          )
-        ).toJson(),
-        infiniteBounds: false),
-        maxZoom: 18,
-        minZoom: 12,
-      )
-    );
+    
+    // mapboxMap.setBounds(CameraBoundsOptions(
+    //   bounds: CoordinateBounds(
+    //     southwest: Point(
+    //       coordinates: Position(
+    //         38.828945,
+    //         47.157511,
+    //       )
+    //     ).toJson(),
+    //     northeast: Point(
+    //       coordinates: Position(
+    //         38.956618,
+    //         47.290974,
+    //       )
+    //     ).toJson(),
+    //     infiniteBounds: false),
+    //     maxZoom: 18,
+    //     minZoom: 12,
+    //   ));
+
+
+    mapboxMap.annotations.createPointAnnotationManager().then((value) async {
+      pointAnnotationManager = value;
+      final ByteData bytes =
+          await rootBundle.load('assets/marker1.jpg');
+      final Uint8List list = bytes.buffer.asUint8List();
+      createOneAnnotation(list);
+      var options = <PointAnnotationOptions>[];
+      for (var i = 0; i < 5; i++) {
+        options.add(PointAnnotationOptions(
+            geometry: createRandomPoint().toJson(), image: list));
+      }
+      options.add(PointAnnotationOptions(
+            geometry: createOnePoint().toJson(), image: list));
+      pointAnnotationManager?.createMulti(options);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+      final MapWidget mapWidget =
+        MapWidget(
+          key: const ValueKey("mapWidget"),
+          resourceOptions: ResourceOptions(accessToken: 'sk.eyJ1IjoiYnJ1aGRyZWRkMTIzIiwiYSI6ImNsc290a2l4dDBoMDgya3A5ZGVpaXk4bHcifQ.yL0JNi5ms28HUhjb0Pg25A'),
+          styleUri: MapboxStyles.DARK,
+          onMapCreated: _onMapCreated,);
+
     return Scaffold(
-        body: MapWidget(
-      key: const ValueKey("mapWidget"),
-      resourceOptions: ResourceOptions(accessToken: 'sk.eyJ1IjoiYnJ1aGRyZWRkMTIzIiwiYSI6ImNsc290a2l4dDBoMDgya3A5ZGVpaXk4bHcifQ.yL0JNi5ms28HUhjb0Pg25A'),
-      onMapCreated: _onMapCreated,
+        body: mapWidget,
+    );
+  }
+
+  Point createOnePoint() {
+    return Point(coordinates: Position(
+      38.934849,
+      47.203127
     ));
+  }
+
+  void createOneAnnotation(Uint8List list) {
+    pointAnnotationManager
+        ?.create(PointAnnotationOptions(
+            geometry: Point(
+                coordinates: Position(
+              38.934849,
+              47.203127
+            )).toJson(),
+            textField: "SISKI",
+            textOffset: [0.0, -2.0],
+            textColor: Colors.red.value,
+            iconSize: 3,
+            iconOffset: [0.0, -5.0],
+            symbolSortKey: 10,
+            image: list))
+        .then((value) => pointAnnotation = value);
   }
 }
---
+++ lib/utils.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+Point createRandomPoint() {
+  return Point(coordinates: createRandomPosition());
+}
+
+Position createRandomPosition() {
+  var random = Random();
+  return Position(random.nextDouble() * -360.0 + 180.0,
+      random.nextDouble() * -180.0 + 90.0);
+}
